77adfd7dd15942b5b9dcb05fb1f3e6284f77908c
Brent Matthew Ortizo <ortizo@chapman.edu>
2025-10-28 19:57:50 -0700

    Test comment

--- src/BAMGame2/Assets/Scripts/TestScript.cs
+++ src/BAMGame2/Assets/Scripts/TestScript.cs
@@ -5,7 +5,7 @@ public class TestScript : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        // Test Comment
+        // Test 123
     }
 
     // Update is called once per frame
