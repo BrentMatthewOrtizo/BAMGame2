5b10bdf8516176c56e25102b758df7342656433c
Brent Matthew Ortizo <ortizo@chapman.edu>
2025-11-06 13:41:24 -0800

    Revised playerwallet

--- src/BAMGame2/Assets/Scripts/Collectible.cs
+++ src/BAMGame2/Assets/Scripts/Collectible.cs
@@ -1,15 +1,47 @@
 using UnityEngine;
 
+public enum CollectibleType
+{
+    Gold,
+    Seed,
+    Crop
+}
+
 [RequireComponent(typeof(Collider2D))]
 public class Collectible : MonoBehaviour
 {
-    private void Reset() => GetComponent<Collider2D>().isTrigger = true;
+    [Header("Collectible Settings")]
+    public CollectibleType type = CollectibleType.Gold;
+    public int amount = 1;
+
+    private void Reset()
+    {
+        Collider2D col = GetComponent<Collider2D>();
+        if (col != null)
+            col.isTrigger = true;
+    }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("Player"))
+        if (!other.CompareTag("Player"))
+            return;
+
+        switch (type)
         {
-            Destroy(gameObject);
+            case CollectibleType.Gold:
+                if (PlayerWallet.Instance != null)
+                    PlayerWallet.Instance.AddGold(amount);
+                break;
+
+            case CollectibleType.Seed:
+                // will hook into inventory later
+                break;
+
+            case CollectibleType.Crop:
+                // will hook into inventory later
+                break;
         }
+
+        Destroy(gameObject);
     }
 }
